16dc57c777073f871669d17f617845fd7656b7b6
quantd2 <dinhquan.tran@gmail.com>
2017-06-07 17:12:50 +0700

    working on footer

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+  get 'companies/index'
+
+  get 'companies/show'
+
+  get 'companies/new'
+
+  get 'companies/edit'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'static_pages#home'
   get "static-pages/products"
