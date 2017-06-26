7442a03f09ec65c65f6a90e0574a08dd914d9828
quantd2 <dinhquan.tran@gmail.com>
2017-06-26 15:35:28 +0700

    add contact form

--- Gemfile
+++ Gemfile
@@ -35,6 +35,8 @@ gem 'jbuilder', '~> 2.5'
 # gem 'capistrano-rails', group: :development
 gem 'bower-rails'
 gem 'slim-rails'
+gem 'mail_form'
+gem 'simple_form'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,14 @@
 Rails.application.routes.draw do
+  get 'contacts/new'
+
+  get 'contacts/create'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'static_pages#home'
   get 'static-pages/cokhichinhxac'
   get 'static-pages/cokhicongnghiep'
   get 'static-pages/about'
   get 'static-pages/home'
+  resources "contacts", only: [:new, :create]
 
 end
