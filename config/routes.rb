d8538b6ff3ab69a1e7810884e945f14f3b81da34
quantd2 <dinhquan.tran@gmail.com>
2017-06-29 12:09:51 +0700

    change robot

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'static_pages#home'
+  root 'static_pages#giacongcokhi'
   get 'static-pages/cokhichinhxac'
   get 'static-pages/cokhicongnghiep'
-  # get 'static-pages/about'
+  get 'static-pages/giacongcokhi'
   # get 'static-pages/home'
   resources "contacts", only: [:new, :create]
 
